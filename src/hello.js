212f0d2cb33c953a573d6b94a5f55c3692fa36f0
ozimiya <ozimiya2@gmail.com>
2019-04-29 21:25:39 +0900

    環境構築

---
+++ src/hello.js
@@ -0,0 +1,27 @@
+/**
+ * 挨拶クラス
+ * (ECMAScript 6 スタイル)
+ * Helloクラスがdefaultのexport対象とする
+ */
+export default class Hello {
+
+    //コンストラクタ
+    constructor() {
+    }
+
+    /**
+     * 挨拶をする
+     * @returns {string}
+     */
+    sayHello() {
+        
+        const hello = 'Hi, there!';
+        console.log(hello);
+
+        return hello;
+    }
+    aaaa(){
+        const aaaaaa = 'いいいいいいい';
+    }
+    
+}
