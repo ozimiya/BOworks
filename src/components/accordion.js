7219c5b1ccaaca63aff036efb836bb73afa4ff57
ozimiya <ozimiya2@gmail.com>
2019-05-02 21:23:16 +0900

    アコーディオン実装・挙動OK  next→機能別に作り直す

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,7 +1,7 @@
 const DATA_TEXT = 'data-text';
 const DATA_IMG = 'data-img';
 const DATA_ACOORDION = 'data-accordion';
-const DN = 'dn';
+const CLASS_OPEN = 'dn';
 
 export default (params) => {
     const openTriggers = Array.from(document.getElementsByClassName(params.openTrigger));
@@ -11,73 +11,72 @@ export default (params) => {
     let imgObj = {};
     const state = {
         otherTrigger: null,
-        show: false,
     }
 
     const actionFunc = (e) => {
-        const trigger = e.currentTarget; 
-        const target = trigger.nextElementSibling;
+        const trigger = e.currentTarget;
+        const mainTrigger = trigger.querySelector('['+ DATA_IMG +']')? true : false;
+        let target = mainTrigger? trigger.nextElementSibling : null; 
 
         if(!Object.keys(textObj).length){
             textObj = getTriggerText(trigger);
             imgObj = getImgPath(trigger);
-            changeState(trigger);
+            saveTriggerForDiff(trigger);
         }
-        toggleAction(trigger, target, textObj, imgObj);
+        toggleAction(trigger, target, textObj, imgObj, mainTrigger);
     };
 
     const getTriggerText = (_trigger) => {
-        const openText =  _trigger.children[0].textContent;
-        const closeText = _trigger.children[0].getAttribute(DATA_TEXT);
+        const text = _trigger.querySelector('['+ DATA_TEXT +']');
+        const openText =  text.textContent;
+        const closeText = text.getAttribute(DATA_TEXT);
         return { open: openText, close: closeText };
     };
 
     const getImgPath = (_trigger) => {
-        const img = _trigger.querySelector('[' + DATA_IMG + ']');
+        const img = _trigger.querySelector('['+ DATA_IMG +']');
         const imgData = img.getAttribute(DATA_IMG);
         const imgSrc = img.getAttribute('src');
         return { open: imgSrc, close: imgData };
     };
 
-    const changeState = (_trigger) => {
+    const saveTriggerForDiff = (_trigger) => {
         state.otherTrigger = _trigger;
     };
 
-    const toggleAction = (_trigger, _target, _text, _img) => {
+    const toggleAction = (_trigger, _target, _text, _img, _isMain) => {
         if(state.otherTrigger !== _trigger) closeOtherParts(_trigger, _text, _img);
-
-        // state.show = !state.show;
     
-        _target.classList.toggle(DN);
-        if(_target.classList.contains(DN)){
-            _trigger.children[0].textContent = _text.open;
-            const img = _trigger.querySelector('[' + DATA_IMG + ']');
+        
+        if(_isMain) _target.classList.toggle(CLASS_OPEN);
+        let img = _trigger.querySelector('[' + DATA_IMG + ']');
+        
+        if(!_isMain) return;
+        if(_target.classList.contains(CLASS_OPEN)){
             img.setAttribute('src', _img.open);
+            _trigger.querySelector('['+ DATA_TEXT +']').textContent = _text.open;
         }else{
-            _trigger.children[0].textContent = _text.close;
-            const img = _trigger.querySelector('[' + DATA_IMG + ']');
             img.setAttribute('src', _img.close);
+            _trigger.querySelector('['+ DATA_TEXT +']').textContent = _text.close;
         }
     };
 
     const closeOtherParts = (_trigger, _text, _img) => {
         for(const target of targets){
-            target.classList.add(DN);
+            target.classList.add(CLASS_OPEN);
         }
         for(const trigger of openTriggers){
-            trigger.children[0].textContent = _text.open;
+            trigger.querySelector('['+ DATA_TEXT +']').textContent = _text.open;
             const img = trigger.querySelector('[' + DATA_IMG + ']');
             img.setAttribute('src', _img.open);
         }
-        changeState(_trigger);
+        saveTriggerForDiff(_trigger);
     };
     
     for(const openTrigger of openTriggers){
         openTrigger.addEventListener('click', actionFunc, false);
     }
-
-    // const changeImgPath = (_img) => {
-    //     img.setAttribute('src', imgData);
-    //     img.setAttribute('data-img', imgSrc);
-    // };
+    for(const closeTrigger of closeTriggers){
+        closeTrigger.addEventListener('click', actionFunc, false);
+    }
 };
